9b2210d562cfc6149607053cf259e70d35a129a2
AceShadowKnight <andrew.blass@temple.edu>
2025-09-03 20:27:13 -0400

    Updating Files

--- src/EquilateralTriangle.kt
+++ src/EquilateralTriangle.kt
@@ -1,3 +1,7 @@
 class EquilateralTriangle (name: String): Triangle(name) {
-
+    fun setDimensions(x: Double){
+        Side_one = x;
+        Side_two = x;
+        Side_three = x;
+    }
 }
---
+++ src/Main.kt
@@ -0,0 +1,2 @@
+class Main (args: Array<String>){
+}
